bfdb8929e0e8c8cc04433fb1695ac3e99bfec3d9
PickleLily <lily.bromberger@gmail.com>
2025-02-28 14:38:40 -0500

    moved encryption functionality

--- backend/objects/Server.py
+++ backend/objects/Server.py
@@ -4,6 +4,12 @@ import json
 import concurrent.futures 
 import uuid #mac address 
 from datetime import datetime #to get the current time 
+import cryptography as c
+from cryptography.hazmat.primitives import serialization, hashes, padding
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import os
 
 from utils.server_util import * 
 
@@ -441,14 +447,133 @@ class Server(object):
 
 
 
-    @staticmethod
-    def decrypt_data(priv_key, ciphertext_message):
-        """Decrypts the given message with the given key"""
-
-        return NotImplementedError
+# ____________________________________--encryption methods--___________________________________________
     
-    @staticmethod
-    def encrypt_data(public_key, plaintext_data):
-        """Encrypts the given data with the given public key."""
 
-        return NotImplementedError  
+    @staticmethod
+    def generate_keys(password):
+        key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=4096)
+
+        
+        private_key = key.private_bytes(
+            serialization.Encoding.PEM,
+            serialization.PrivateFormat.PKCS8,
+            encryption_algorithm= serialization.BestAvailableEncryption(password)
+        )
+
+        public_key = key.public_key().public_bytes(
+            serialization.Encoding.PEM,
+            serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+        # generates a random 256 bit key... 
+        key = os.urandom(32)
+        iv = os.urandom(16)
+        AES_doer = Cipher(algorithms.AES(key), modes.CBC(iv))
+        
+        return(public_key, private_key, AES_doer)
+
+
+    @staticmethod
+    def save_key(private_key, public_key):
+        
+        with open("private.pem", "wb") as pem:
+            pem.write(private_key)
+
+        with open("public.pem", "wb") as pempub:
+            pempub.write(public_key)
+
+
+    @staticmethod
+    def recover_private_key(password):
+        try:
+            with open("private.pem", "rb") as key_file:
+                private_key = serialization.load_pem_private_key(
+                    data=key_file.read(),
+                    password=password, 
+                    backend=default_backend
+                )
+            return private_key
+        
+        except Exception as e:
+            print(f"Error opening PEM file: {e}")
+            return None
+        
+
+    @staticmethod
+    def recover_public_key():
+        try:
+            with open("public.pem", "rb") as key_file:
+                public_key = serialization.load_pem_public_key(
+                    data=key_file.read(),
+                    backend=default_backend
+                )
+            return public_key
+        
+        except Exception as e:
+            print(f"Error opening PEM file: {e}")
+            return None
+
+
+    @staticmethod
+    def parse_data(file):
+        with open(file, "rb") as data:
+            parsed_data = data.read()  
+
+        return parsed_data
+
+
+
+    @staticmethod
+    def RSA_encrypt(data, key):
+        cipher = key.encrypt(data,
+                    padding.OAEP(
+                        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                        algorithm=hashes.SHA256(),
+                        label=None
+                    )
+                )
+        return cipher
+
+
+    @staticmethod
+    def RSA_decrypt(data, key):
+        message = key.decrypt(data,
+                    padding.OAEP(
+                        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                        algorithm=hashes.SHA256(),
+                        label=None  
+                    )
+                )
+
+        return message
+
+    @staticmethod
+    def AES_encrypt(data, doer):
+        padder = c.hazmat.primitives.padding.PKCS7(128).padder()
+        padded_data = padder.update(data)
+        padded_data += padder.finalize()
+
+        encryptor = doer.encryptor()
+        cipher = encryptor.update(padded_data) + encryptor.finalize()
+        return cipher
+
+    @staticmethod
+    def AES_decrypt(data, doer):
+        decryptor = doer.decryptor()
+        text = decryptor.update(data) + decryptor.finalize() 
+
+        unpadder = c.hazmat.primitives.padding.PKCS7(128).unpadder()
+        unpadded_data = unpadder.update(text)
+        unpadded_data += unpadder.finalize()
+        return  unpadded_data
+
+    # TODO -- this is untested...
+    def hashMessage(data):
+        digest = hashes.Hash(hashes.SHA256())
+        digest.update(data)
+        digest.finalize()
+        return digest
+
