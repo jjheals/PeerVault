74a2ca8ffec7267a2c6c6ab26b79e830c3cfb607
jjheals <jjheals@gmail.com>
2025-03-16 11:48:50 -0400

    fix load_key() -> load_key_pem()

--- backend/blueprints/frontend_interaction.py
+++ backend/blueprints/frontend_interaction.py
@@ -5,7 +5,7 @@ import pandas as pd
 from configparser import ConfigParser
 from hashlib import sha256
 
-from utils import filter_args, load_key, get_mac_address
+from utils import filter_args, load_key_pem, get_mac_address
 from .funcs import require_localhost
 
 # ---- Config & init ---- #
@@ -164,7 +164,7 @@ def whoami():
     identity_config.read('config/identity.conf')
                 
     # Load this user's public key
-    pub_key:str = load_key(
+    pub_key:str = load_key_pem(
         current_app.enc_config['paths']['PUB_KEY_PATH'],
         'public'
     )
