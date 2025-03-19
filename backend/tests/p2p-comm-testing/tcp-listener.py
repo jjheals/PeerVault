a26ed21c0a213d2eb40e410273648d71dd59ea69
jjheals <jjheals@gmail.com>
2025-03-19 13:24:40 -0400

    improve tcp tests

--- backend/tests/p2p-comm-testing/tcp-listener.py
+++ backend/tests/p2p-comm-testing/tcp-listener.py
@@ -1,13 +1,74 @@
 import socket
+from configparser import ConfigParser
+from utils import now
+import json 
 
+# NOTE: actual codes are defined in the Server obj (objects/Server.py)
+DISC_CODE:str = "000"
+IDC_CODE:str = "001"
+SEND_REQ_CODE:str = "101"
+STORE_REQ_CODE:str = "102"
+DEL_FILE_CODE:str = "103"
+UPD_FILE_CODE:str = "104" 
+
+
+
+# --- Load config --- #
+# Network config for listener interface and port 
+network_config:ConfigParser = ConfigParser()
+network_config.read('config/network.conf')
+
+# Extract the listener interface and port from the network config 
+IFACE:str = network_config['network']['IFACE']
+PORT:int = int(network_config['network']['PEER_PORT'])
+
+# Encryption config for identity check
+enc_config:ConfigParser = ConfigParser() 
+enc_config.read('config/encryption.conf') 
+
+
+# --- Init listener socket --- #
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("0.0.0.0", 5005))
+server_socket.bind((IFACE, PORT))
 server_socket.listen(1)
 
-print("Waiting for a connection...")
-conn, addr = server_socket.accept()
-print(f"Connection from {addr}")
 
-data = conn.recv(1024)
-print(f"Received: {data.decode()}")
-conn.close()
+# --- Listen for incomming messages --- #
+while True: 
+    print(f'\n\033[0m[{now()}] \033[94mWaiting for a connection...\033[0m')
+
+    # Accept an incoming message
+    conn, addr = server_socket.accept()
+
+    # Print a connection message when connection occurs
+    print(f'\033[0m[{now()}] \033[92mIncoming connection\033[0m')
+    print(f'\n\t\033[0mPeer (IP, PORT): {addr}')
+
+    # Read the incoming data, decode, and print to terminal
+    data = conn.recv(1024)
+    print(f'\t\033[0mMessage: "{data.decode()}"')
+
+    # -- Handle the incoming message -- # 
+    # Extract the JSON and conver to a python dict
+    message_json:dict = json.loads(data.decode())
+    print(f'\t\033[0mExtracted message JSON: {message_json}')
+
+    # Check for a code 
+    code:int = message_json.get('code', None)
+
+    # If no code, ignore the message and close the cxn
+    if not code: 
+        print('\n\t\033[91mERROR: \033[0mmessage does not contain a "code" - ignoring message.\033[0m')
+        conn.close()
+        continue
+    
+    # If there is a code, handle it appropriately
+    if code == DISC_CODE: 
+        print('\n\t\033[92mIdentified code: \033[0m"DISCOVERY"')
+    elif code == IDC_CODE: 
+        print('\t\033[92mIdentified code: \033[0m"IDENTITY CHECK"')
+
+    else: 
+        print(f'\n\t\033[91mERROR: \033[0mmessage contains an unrecognized code "{code}")')
+
+    conn.close()
--- backend/tests/p2p-comm-testing/tcp-sender.py
+++ backend/tests/p2p-comm-testing/tcp-sender.py
@@ -1,9 +1,43 @@
 import socket
+from configparser import ConfigParser
+import json 
 
+# NOTE: actual codes are defined in the Server obj (objects/Server.py)
+DISC_CODE:str = "000"
+IDC_CODE:str = "001"
+SEND_REQ_CODE:str = "101"
+STORE_REQ_CODE:str = "102"
+DEL_FILE_CODE:str = "103"
+UPD_FILE_CODE:str = "104" 
+
+
+# --- Load config --- #
+# Network config for the target port
+network_config:ConfigParser = ConfigParser()
+network_config.read('config/network.conf')
+
+# Encryption config for identity check
+enc_config:ConfigParser = ConfigParser()
+enc_config.read('config/encryption.conf')
+
+# Define target IP and get the port from the network config
+TARGET_IP:str = '127.0.0.1'
+PORT:int = int(network_config['network']['PEER_PORT'])
+
+
+# --- Init socket --- #
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(("192.168.1.100", 5005))
+client_socket.connect((TARGET_IP, PORT))
 
-message = "Hello, TCP packet!"
-client_socket.send(message.encode())
 
+# --- Construct and send message --- #
+# Construct the json body to send
+message_dict:dict = {
+    'code': DISC_CODE
+}
+
+# Send the message
+client_socket.send(json.dumps(message_dict).encode())
+
+# Close cxn
 client_socket.close()
