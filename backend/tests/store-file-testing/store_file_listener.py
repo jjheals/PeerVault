a1d947694749226f8fa6e6e9935081c910eee55b
jjhealey <jjheals@gmail.com>
2025-04-23 16:53:09 -0400

    update store file tests to use new Server constructor and configs

--- backend/tests/store-file-testing/store_file_listener.py
+++ backend/tests/store-file-testing/store_file_listener.py
@@ -12,33 +12,54 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 sys.path.insert(0, parent_dir)
 
 # Util and object imports
-from utils import load_key_pem, load_aes_key
-from objects import Server
+from utils import load_key_pem, load_aes_key, load_configs
+from objects import Server, DatabaseConnection
+
 
 # --- Config --- #
-# Network config
-network_config:ConfigParser = ConfigParser()
-network_config.read('../../config/network.conf')
+# NOTE: loading info for the LISTENER 
+# NOTE: define the passphrase for loading the keys
+PASSPHRASE:str = 'i_am_the_listener'
+
+# Load the configs
+config_parsers:dict[str, ConfigParser] = load_configs('../TEST-config/listener/')
+for k,v in config_parsers.items(): print(f'{k} : {v}')
+
+# Extract vars from network config
+network_config:ConfigParser = config_parsers['network']
 
 MCAST_GRP = network_config['multicast']['MCAST_GROUP']       # Multicast group addr
 MCAST_PORT = int(network_config['multicast']['MCAST_PORT'])  # Port to listen on
-IFACE = "127.0.0.1" #network_config['multicast']['LOCAL_IP']              # Local IP
-PORT = int(network_config['network']['PORT'])
+IFACE = network_config['multicast']['LOCAL_IP']              # Local IP
+PORT = int(network_config['network']['PORT'])                # Server port
 
-# Identity config
-identity_config:ConfigParser = ConfigParser()
-identity_config.read('../../config/identity.conf')
+# Extract vars from identity config
+identity_config:ConfigParser = config_parsers['identity']
 
-COMMON_NAME:str = identity_config['IDENTITY']['common_name']    # Common name
+COMMON_NAME:str = identity_config['IDENTITY']['common_name']            # Common name
+PEER_STORAGE_DIR:str = identity_config['PATHS']['peer_storage_path']    # Peer storage path
 
-# Encryption config 
-# NOTE: using static paths relative to this script rather than the paths in enc config
-#enc_config:ConfigParser = ConfigParser()
-#enc_config.read('../../config/encryption.conf')
+# Extract vars from encryption config 
+enc_config:ConfigParser = config_parsers['encryption']
 
-PUBLIC_KEY_PEM:str = load_key_pem('../TEST-keys/TEST-public.key', 'public')
-PRIV_KEY_PEM:str = load_key_pem('../TEST-keys/TEST-private.key', 'private', 'SomeSuperSecurePassphrase')
-SYMM_KEY:str = load_aes_key('SomeSuperSecurePassphrase', '../TEST-keys/TEST-symm.key')
+PUBLIC_KEY_PEM:str = load_key_pem(enc_config['paths']['pub_key_path'], 'public')
+PRIV_KEY_PEM:str = load_key_pem(enc_config['paths']['priv_key_path'], 'private', PASSPHRASE)
+SYMM_KEY:str = load_aes_key(PASSPHRASE, enc_config['paths']['symm_key_path'])
+
+# Extract vars from flask config
+flask_config:ConfigParser = config_parsers['flask']
+
+LOGS_DIR:str = flask_config['paths']['LOGS_DIR']    # Path to the dir to output logs
+DB_PATH:str = flask_config['paths']['DB_PATH']      # Path to the test DB
+
+# Construct rel paths from the vars from flask config
+DB_LOG_PATH:str = os.path.join(LOGS_DIR, 'store', 'listener-database.log')     # Path to output DB logs
+SERVER_LOG_PATH:str = os.path.join(LOGS_DIR, 'store', 'listener-server.log')   # Path to output server logs
+
+# Create unique names for the loggers so they don't interfere with other scripts
+SERVER_LOGGER_NAME:str = 'store_listener_server_logger'  # Name for the server logger
+DB_LOGGER_NAME:str = 'store_listener_db_logger'          # Name for the DB logger  
+
 
 # --- Init --- #
 # Init a Server obj
@@ -52,10 +73,18 @@ server:Server = Server(
     IFACE,                  # mcast_iface
     MCAST_PORT,             # mcast_port
     MCAST_GRP,              # mcast_group
-    '../../peer-info/',     # data_dir_path
-    '.'                     # peer_storage_path
+    DatabaseConnection(     # send_db_connection
+        DB_PATH, 
+        DB_LOG_PATH, 
+        logger_name=DB_LOGGER_NAME
+    ),
+    DB_PATH,                            # db_filepath
+    PEER_STORAGE_DIR,                   # peer_storage_dir
+    log_filepath=SERVER_LOG_PATH,       # log_filepath
+    logger_name=SERVER_LOGGER_NAME,     # logger_name
+    db_log_filepath=DB_LOG_PATH,        # db_log_filepath
+    db_logger_name=DB_LOGGER_NAME       # db_logger_name
 )
-
 
 
 # --- Start tcp listener --- #
