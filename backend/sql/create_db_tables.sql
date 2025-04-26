1475349151c0e993e9159d725fa497fc94b617d6
jjheals <jjheals@gmail.com>
2025-04-26 13:11:53 -0400

    add 'accepted' field to PendingRequests table

--- backend/sql/create_db_tables.sql
+++ backend/sql/create_db_tables.sql
@@ -61,7 +61,8 @@ CREATE TABLE PendingRequests(
     filename TEXT NOT NULL,
     size_gb REAL NOT NULL,
     sha256 TEXT NOT NULL,
-    request_date TEXT NOT NULL,     -- Must be in [YYYY-MM-DD] format 
+    accepted BOOLEAN DEFAULT NULL,     -- Whether or not the request has been accepted (for outgoing reqs, the peer accepts; for incoming reqs, we accept)
+    request_date TEXT NOT NULL,        -- Must be in [YYYY-MM-DD] format 
     CHECK (request_date GLOB '[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]'),
     CHECK (direction = 'incoming' OR direction = 'outgoing'),
     CHECK (request_type IN ('store', 'share', 'delete', 'retrieve')),
