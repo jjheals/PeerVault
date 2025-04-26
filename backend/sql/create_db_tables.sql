57cda543889895c8f703ec32ab33d732c3ecb19c
jjheals <jjheals@gmail.com>
2025-04-26 13:35:50 -0400

    add CompletedRequests table

--- backend/sql/create_db_tables.sql
+++ backend/sql/create_db_tables.sql
@@ -54,7 +54,7 @@ CREATE TABLE PreviouslySharedWith(
 
 /* Table for pending requests that have not yet been processed */
 CREATE TABLE PendingRequests(
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     direction TEXT NOT NULL,       -- Must be either [incoming | outgoing]
     request_type TEXT NOT NULL,    -- Must be one of [store | share | delete]
     peer_pub_key TEXT NOT NULL,
@@ -68,3 +68,21 @@ CREATE TABLE PendingRequests(
     CHECK (request_type IN ('store', 'share', 'delete', 'retrieve')),
     FOREIGN KEY (peer_pub_key) REFERENCES Peer(peer_pub_key)
 );
+
+
+/* Table for completed requests that have been processed or declined (basically a copy of PendingRequests) */
+CREATE TABLE CompletedRequests(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    direction TEXT NOT NULL,       -- Must be either [incoming | outgoing]
+    request_type TEXT NOT NULL,    -- Must be one of [store | share | delete]
+    peer_pub_key TEXT NOT NULL,
+    filename TEXT NOT NULL,
+    size_gb REAL NOT NULL,
+    sha256 TEXT NOT NULL,
+    accepted BOOLEAN DEFAULT NULL,     -- Whether or not the request has been accepted (for outgoing reqs, the peer accepts; for incoming reqs, we accept)
+    request_date TEXT NOT NULL,        -- Must be in [YYYY-MM-DD] format 
+    CHECK (request_date GLOB '[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]'),
+    CHECK (direction = 'incoming' OR direction = 'outgoing'),
+    CHECK (request_type IN ('store', 'share', 'delete', 'retrieve')),
+    FOREIGN KEY (peer_pub_key) REFERENCES Peer(peer_pub_key)
+)
