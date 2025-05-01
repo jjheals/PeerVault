87890f07c02258f5d6a6f96585605fc63f86dcd6
jjheals <jjheals@gmail.com>
2025-05-01 11:43:00 -0400

    update mcast tests

--- backend/tests/multicast-testing/mcast_listener.py
+++ backend/tests/multicast-testing/mcast_listener.py
@@ -1,5 +1,6 @@
 import os 
 from configparser import ConfigParser
+import threading as th 
 
 # Modify sys path to import utils 
 import sys
@@ -11,57 +12,87 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 sys.path.insert(0, parent_dir)
 
 # Util and object imports
-from utils import load_key_pem
+from utils import load_key_pem, load_aes_key, load_configs
 from objects import Server, DatabaseConnection
 
 
 # --- Config --- #
-# Network config
-network_config:ConfigParser = ConfigParser()
-network_config.read('../../config/network.conf')
+# NOTE: loading info for the LISTENER 
+# NOTE: define the passphrase for loading the keys
+PASSPHRASE:str = 'i_am_the_listener'
+
+# Load the configs
+config_parsers:dict[str, ConfigParser] = load_configs('TEST-config/listener/')
+for k,v in config_parsers.items(): print(f'{k} : {v}')
+
+# Extract vars from network config
+network_config:ConfigParser = config_parsers['network']
 
 MCAST_GRP = network_config['multicast']['MCAST_GROUP']       # Multicast group addr
 MCAST_PORT = int(network_config['multicast']['MCAST_PORT'])  # Port to listen on
 IFACE = network_config['multicast']['LOCAL_IP']              # Local IP
-PORT = int(network_config['network']['PORT'])
+PORT = int(network_config['network']['PORT'])                # Server port
 
-# Identity config
-identity_config:ConfigParser = ConfigParser()
-identity_config.read('../../config/identity.conf')
+# Extract vars from identity config
+identity_config:ConfigParser = config_parsers['identity']
 
-COMMON_NAME:str = identity_config['IDENTITY']['common_name']    # Common name
+COMMON_NAME:str = identity_config['IDENTITY']['common_name']            # Common name
+PEER_STORAGE_DIR:str = identity_config['PATHS']['peer_storage_path']    # Peer storage path
 
-# Encryption config 
-# NOTE: using static paths relative to this script rather than the paths in enc config
-#enc_config:ConfigParser = ConfigParser()
-#enc_config.read('../../config/encryption.conf')
+# Extract vars from encryption config 
+enc_config:ConfigParser = config_parsers['encryption']
 
-PUBLIC_KEY_PEM:str = load_key_pem('../TEST-keys/TEST-public.key', 'public')
-PRIV_KEY_PEM:str = load_key_pem('../TEST-keys/TEST-private.key', 'private', 'SomeSuperSecurePassphrase')
+PUBLIC_KEY_PEM:str = load_key_pem(enc_config['paths']['pub_key_path'], 'public')
+PRIV_KEY_PEM:str = load_key_pem(enc_config['paths']['priv_key_path'], 'private', PASSPHRASE)
+SYMM_KEY:str = load_aes_key(PASSPHRASE, enc_config['paths']['symm_key_path'])
 
-# NOTE: defining other static paths for testing 
-DB_PATH:str = '../TEST-data/TEST-database.db'
-DB_LOG_PATH:str = '../TEST-logs/TEST-database.log'
-SERVER_LOG_PATH:str = '../TEST-logs/TEST-server.log'
-PEER_STORAGE_DIR:str = '../TEST-peer-storage/'
+# Extract vars from flask config
+flask_config:ConfigParser = config_parsers['flask']
+
+LOGS_DIR:str = flask_config['paths']['LOGS_DIR']    # Path to the dir to output logs
+DB_PATH:str = flask_config['paths']['DB_PATH']      # Path to the test DB
+
+# Construct rel paths from the vars from flask config
+DB_LOG_PATH:str = os.path.join(LOGS_DIR, 'store', 'listener-database.log')     # Path to output DB logs
+SERVER_LOG_PATH:str = os.path.join(LOGS_DIR, 'store', 'listener-server.log')   # Path to output server logs
+
+# Create unique names for the loggers so they don't interfere with other scripts
+SERVER_LOGGER_NAME:str = 'store_listener_server_logger'  # Name for the server logger
+DB_LOGGER_NAME:str = 'store_listener_db_logger'          # Name for the DB logger  
+
 
 # --- Init --- #
 # Init a Server obj
 server:Server = Server(
     PUBLIC_KEY_PEM,         # pub_key_pem
     PRIV_KEY_PEM,           # priv_key_pem
-    '',                     # symm_aes_key - NOTE: symm AES key doesn't matter for this test script
+    SYMM_KEY,               # symm_aes_key
     COMMON_NAME,            # common_name
     IFACE,                  # iface
     PORT,                   # port
     IFACE,                  # mcast_iface
     MCAST_PORT,             # mcast_port
     MCAST_GRP,              # mcast_group
-    DatabaseConnection(DB_PATH, DB_LOG_PATH),  # db_connection
-    PEER_STORAGE_DIR,                          # peer_storage_dir
-    log_filepath=SERVER_LOG_PATH               # log_filepath
+    DatabaseConnection(     # send_db_connection
+        DB_PATH, 
+        DB_LOG_PATH, 
+        logger_name=DB_LOGGER_NAME
+    ),
+    DB_PATH,                            # db_filepath
+    PEER_STORAGE_DIR,                   # peer_storage_dir
+    log_filepath=SERVER_LOG_PATH,       # log_filepath
+    logger_name=SERVER_LOGGER_NAME,     # logger_name
+    db_log_filepath=DB_LOG_PATH,        # db_log_filepath
+    db_logger_name=DB_LOGGER_NAME,      # db_logger_name
+    temp_dir='.tmp/listener'
 )
 
-# --- Multicast listen --- #
-# Start the server mcast listener
-server.mcast_listen()
+
+# --- Start tcp listener --- #
+server.server_alive = True
+
+# Define thread for the listener
+listen_thread:th.Thread = th.Thread(target=server.mcast_listen)
+
+# Start the listener threads
+listen_thread.start()
