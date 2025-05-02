c71c58cd9cce2aa836bea3d78d4ca5752f04a785
PickleLily <lily.bromberger@gmail.com>
2025-05-02 14:45:33 -0400

    added data validation, signout, and logo

--- frontend/src/app/createAccount/page.tsx
+++ frontend/src/app/createAccount/page.tsx
@@ -17,7 +17,9 @@ export default function Home() {
     const [model, setModel] = React.useState(new Model())
     const [redraw, forceRedraw] = React.useState(0);
     const [username, setUsername] = React.useState("");
-    const [passphrase, setPassphrase] = React.useState("");
+    const [passphrase1, setPassphrase1] = React.useState("");
+    const [passphrase2, setPassphrase2] = React.useState("");
+
     const [storage, setStorage] = React.useState("");
     const [path, setPath] = React.useState("");
 
@@ -29,26 +31,42 @@ export default function Home() {
     const router = useRouter();
 
     const handleSignup = async () => {
-      if (!username.trim() ||!passphrase.trim() || !storage.trim()|| !path.trim()) {
+      if (!username.trim() ||!passphrase1.trim() ||!passphrase2.trim()|| !storage.trim()|| !path.trim()) {
           alert("All Fields are required!");
           return;
       }
+      if(passphrase1.trim() != passphrase2.trim()){
+        alert("passphrases do NOT match.");
+        return;
+      }
+
+      const storage_amt = Number(storage.trim());
+      //if storage is a negative number or not a number
+      if(isNaN(storage_amt) || storage_amt < -1){
+        alert("storage amount must be greater or equalto 0 GB.")
+      }
       try {
         const response = await instance.post("/ui/signup", { common_name: username,
-                                                              passphrase: passphrase,
+                                                              passphrase: passphrase1,
                                                               allocated_storage: storage,
                                                               peer_storage_path: path });
 
         alert("signup successful");
       } catch (error) {
-        const statuscode = error.response.status
-        switch (statuscode){
-          case 400:
-            alert("Bad Request. There is missing or misformated data")
-          case 409:
-            alert("A user already exists for this machine")
-          default:
-            alert("Signup failed. Please try again.");
+        if(error.response){
+          const statuscode = error.response.status
+          switch (statuscode){
+            case 400:
+              alert("Bad Request. There is missing or misformated data");
+              break;
+            case 409:
+              alert("A user already exists for this machine");
+              break;
+            default:
+              alert("Signup failed. Please try again.");
+          }
+        }else{
+          alert("Signup failed. Please try again.");
         }
 
       }
@@ -67,14 +85,19 @@ export default function Home() {
           </div>
         </div>
 
-        <div>
-            <div className="subtitleText">Signup</div>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-            <input type="text" value={passphrase} onChange={(e) => setPassphrase(e.target.value)} placeholder="Passphrase" />
-            <input type="int" value={storage} onChange={(e) => setStorage(e.target.value)} placeholder="Storage Amount GB" />
-            <input type="text" value={path} onChange={(e) => setPath(e.target.value)} placeholder="Storage Path" />
-            <button className="button" onClick={handleSignup}>Sign Up</button>
-        </div>
+<div className="signup-form">
+  <div className="subtitleText">Signup</div>
+  <input type="text" value={username}  onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+  <input type="password" value={passphrase1} onChange={(e) => setPassphrase1(e.target.value)} placeholder="Passphrase" />
+  <input type="password" value={passphrase2}  onChange={(e) => setPassphrase2(e.target.value)}  placeholder="Repeat Passphrase" />
+  <input type="number" value={storage} onChange={(e) => setStorage(e.target.value)} placeholder="Storage Amount GB"/>
+  <input type="text" value={path} onChange={(e) => setPath(e.target.value)} placeholder="Storage Path"/>
+
+  <button className="button" onClick={handleSignup}>Sign Up</button>
+</div>
+
+
+
       </div>
     )
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -76,8 +76,6 @@ export default function Home() {
 
     const handleSignOut = async () => {
       setVerifiedUser(false);
-      setIdentity({})
-      setPassphrase("")
       refresh();
     };
 
