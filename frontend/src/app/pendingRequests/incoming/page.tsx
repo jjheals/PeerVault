c0154b28962eab39fa70dd71ce55dd4e698d3360
dacapra <dacapra@optonline.net>
2025-05-02 12:37:00 -0400

    Update pending requests to use peer common name

--- frontend/src/app/pendingRequests/incoming/page.tsx
+++ frontend/src/app/pendingRequests/incoming/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
                 requestData
                 .map((request, index) => (
                 <tr key={index} className="hover:bg-gray-100">
-                  <td className="border p-2 text-center">{request.peer_pub_key}</td>
+                  <td className="border p-2 text-center">{request.peer_cn}</td>
                   <td className="border p-2 text-center">{request.isDirect ? "Direct" : "Universal"}</td>
                   <td className="border p-2 text-center">{request.filename}</td>
                   <td className="border p-2 text-center">{request.size_gb} GB</td>
--- frontend/src/app/pendingRequests/sent/page.tsx
+++ frontend/src/app/pendingRequests/sent/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
           <tbody>
             {requestData.map((request, index) => (
               <tr key={index} className="hover:bg-gray-100">
-                <td className="border p-2 text-center">{request.peer_pub_key}</td>
+                <td className="border p-2 text-center">{request.peer_cn}</td>
                 <td className="border p-2 text-center">{request.filename}</td>
                 <td className="border p-2 text-center">{request.request_type}</td>
                 <td className="border p-2 text-center">{request.size_gb} GB</td>
